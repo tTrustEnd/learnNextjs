3375d040095e9a3631fddd0bb62a38bbe51fbcdd
Truong <banoianh1x@gmail.com>
2023-07-01 10:40:33 +0700

    link router

--- src/app/about/gem/page.tsx
+++ src/app/about/gem/page.tsx
@@ -1,9 +1,12 @@
+"use client" 
+import { useRouter } from "next/navigation";
 import * as React from "react";
 
 export interface IAboutProps {}
 
 export default function About(props: IAboutProps) {
+  const router = useRouter()
   return <div>hello gem
-    
+       <button type="button" onClick={() => {router.push('/about')}}>router.push</button>
   </div>;
 }
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,12 +1,18 @@
+"use client" 
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import * as React from 'react';
-
 export interface IAboutProps {
 }
 
 export default function About (props: IAboutProps) {
+ const router = useRouter()
   return (
     <div>
       hello aboutss
+      <Link href="/about">Link</Link>
+      Dashboard
+      <button type="button" onClick={() => {router.push('/about/gem')}}>router.push = gem</button>
     </div>
   );
 }
